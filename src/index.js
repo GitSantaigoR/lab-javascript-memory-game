419399022718a251ec9f8a0b43afff4ba9284ee9
Santiago Rivera <santiagorockshop1996@hotmail.com>
2023-09-08 09:53:03 -0400

    Lab done

--- src/index.js
+++ src/index.js
@@ -24,9 +24,7 @@ const cards = [
   { name: 'the avengers', img: 'the-avengers.jpg' },
   { name: 'thor', img: 'thor.jpg' }
 ];
-
 const memoryGame = new MemoryGame(cards);
-
 window.addEventListener('load', (event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
@@ -37,15 +35,38 @@ window.addEventListener('load', (event) => {
       </div>
     `;
   });
-
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
-
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
-  });
-});
+    if (memoryGame.pickedCards.length < 2) {
+      memoryGame.pickedCards.push(card);
+      card.classList.toggle("turned");
+    }
+      if (memoryGame.pickedCards.length === 2) {
+        const pairs = memoryGame.checkIfPair(
+          memoryGame.pickedCards[0].getAttribute("data-card-name"),
+          memoryGame.pickedCards[1].getAttribute("data-card-name")
+        );
+        if (pairs) {
+          memoryGame.pickedCards[0].classList.add("blocked");
+          memoryGame.pickedCards[1].classList.add("blocked");
+          const isFinished = memoryGame.checkIfFinished();
+          if (isFinished) {
+            alert("You won!!!!😮💰");
+          }
+          memoryGame.pickedCards = [];
+          return;
+        } else {
+          setTimeout(() => {
+            memoryGame.pickedCards[0].classList.remove("turned");
+            memoryGame.pickedCards[1].classList.remove("turned");
+            memoryGame.pickedCards = [];
+          }, 3000);
+        }
+      }
+      console.log(`Card clicked:`, card);
+      });
+      });
+      });
